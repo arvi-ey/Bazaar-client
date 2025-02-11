9d35dad9fd7b40a0ec5fb4b8c75197536bf599c4
arvi-ey <tuhinroyand@gmail.com>
2025-02-11 23:38:26 +0530

    chore:
    Grid layout completed , navbar and filterbar position design fixed

--- src/Components/Products/Productbox.jsx
+++ src/Components/Products/Productbox.jsx
@@ -1,14 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 // import "../HomeProducts/HomeProduct.css"
 import GradeIcon from '@mui/icons-material/Grade';
+import LocalShippingIcon from '@mui/icons-material/LocalShipping';
 const Productbox = ({ products }) => {
+
+    const [imageFocus, SetImageFocus] = useState(false)
 
     const randomDecimal = (Math.random() * 0.9);
 
+    const GetDeliveryDate = (days) => {
+        if (!days) return
+        const today = new Date();
+        today.setDate(today.getDate() + days);
+        const options = {
+            weekday: 'short',
+            month: 'short',
+            day: 'numeric'
+        };
+        return today.toLocaleDateString('en-US', options);
+    }
+
+
     return (
-        <div className="productBox">
-            <div style={{ position: "relative" }}>
-                <img src={products.images[0]} alt={products.category} className='productImage' />
+        <div className="productBoxs">
+            <div style={{ position: "relative", overflow: "hidden" }}>
+                <img src={imageFocus ? products.images[1] : products.images[0]} alt={products.category} className='productImages' onMouseOver={() => SetImageFocus(true)} onMouseOut={() => SetImageFocus(false)} />
                 <div className='rating'>
                     <GradeIcon sx={{ color: "#FFD700" }} />
                     <p className='ratingText'>
@@ -16,8 +32,8 @@ const Productbox = ({ products }) => {
                     </p>
                 </div>
             </div>
-            <div className='detailBox'>
-                <p className='productTitle'>{products.title}</p>
+            <div className='detailBoxs'>
+                <p className='productTitle'>{products.title.length > 30 ? `${products.title.substring(0, 30)}...` : products.title}</p>
                 <p className='productdesc'>{products.description.substring(0, 55)}...</p>
                 <div style={{ display: "flex", alignItems: "center", }}>
                     <p className='productPrice'>
@@ -35,6 +51,10 @@ const Productbox = ({ products }) => {
                     <p className='discountClass' >
                         {parseInt(randomDecimal * 100)}%OFF
                     </p>
+                </div>
+                <div className='DeliveryDetail'>
+                    <LocalShippingIcon sx={{ color: '#ec0d75' }} />
+                    <p className='DeliveryDetailText'>Free Delivery by {GetDeliveryDate(products.deliveryTime)}</p>
                 </div>
             </div>
         </div>
