7d006314efea0ebaebbabfc8ef40f6ddaff35de3
arvi-ey <tuhinroyand@gmail.com>
2025-08-19 21:05:13 +0530

    Making home page responsive done

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -60,41 +60,43 @@ const Footer = () => {
                         <img src={Appstore} className='downLoadIMage' />
                     </div>
                 </div>
-                <div className="div2">
-                    <p className='div2Title'>SHOP</p>
-                    <div className='div2Contect'>
-                        <p className='div2Text'>The Conrol Kit</p>
-                        <p className='div2Text'>Cleansers & Removers</p>
-                        <p className='div2Text'>Bonds & Sellers</p>
-                        <p className='div2Text'>Gift Sets & Merch</p>
+                <div className='second-div'>
+                    <div className="div2">
+                        <span className='div2Title'>SHOP</span>
+                        <div className='div2Contect'>
+                            <p className='div2Text'>The Conrol Kit</p>
+                            <p className='div2Text'>Cleansers & Removers</p>
+                            <p className='div2Text'>Bonds & Sellers</p>
+                            <p className='div2Text'>Gift Sets & Merch</p>
+                        </div>
                     </div>
-                </div>
-                <div className="div2">
-                    <p className='div2Title'>LEARN</p>
-                    <div className='div2Contect'>
-                        <p className='div2Text'>Book a 1:1</p>
-                        <p className='div2Text'>Video Tutorial</p>
-                        <p className='div2Text'>Live Streaming</p>
-                        <p className='div2Text'>Help & FAQ</p>
-                        <p className='div2Text'>Blog</p>
+                    <div className="div2">
+                        <span className='div2Title'>LEARN</span>
+                        <div className='div2Contect'>
+                            <p className='div2Text'>Book a 1:1</p>
+                            <p className='div2Text'>Video Tutorial</p>
+                            <p className='div2Text'>Live Streaming</p>
+                            <p className='div2Text'>Help & FAQ</p>
+                            <p className='div2Text'>Blog</p>
+                        </div>
                     </div>
-                </div>
-                <div className="div2">
-                    <p className='div2Title'>ABOUT</p>
-                    <div className='div2Contect'>
-                        <p className='div2Text'>About Bazaar</p>
-                        <p className='div2Text'>Press</p>
-                        <p className='div2Text'>Patents & IP</p>
-                        <p className='div2Text'>Shipping & Return</p>
-                        <p className='div2Text'>Contact us</p>
+                    <div className="div2">
+                        <span className='div2Title'>ABOUT</span>
+                        <div className='div2Contect'>
+                            <p className='div2Text'>About Bazaar</p>
+                            <p className='div2Text'>Press</p>
+                            <p className='div2Text'>Patents & IP</p>
+                            <p className='div2Text'>Shipping & Return</p>
+                            <p className='div2Text'>Contact us</p>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div className='LastDiv'>
+            {/* <div className='LastDiv'>
                 <p className='lastDivText'>2025, Bazaar, All rights reservs</p>
                 <p className='lastDivText'>Privacy</p>
                 <p className='lastDivText'>Terms</p>
-            </div>
+            </div> */}
         </div>
     )
 }
